5bdd58c33041089aa7f3dacf2bb57764e8681676
Mark Miklash <markmiklash@gmail.com>
2023-09-21 21:19:34 +0300

    update local master

---
+++ src/main/java/by/it_academy/jd2/mk_jd2_103_23/group3/messenger/controllers/UserRegistrationServlet.java
@@ -0,0 +1,17 @@
+package by.it_academy.jd2.mk_jd2_103_23.group3.messenger.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet
+public class UserRegistrationServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}

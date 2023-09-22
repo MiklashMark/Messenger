fc48a63ba81a9220942536408850f99912cc2ec4
Dima Luschik <dmitry.luschick@yandex.by>
2023-09-23 00:09:27 +0300

    Added UserDao

---
+++ src/main/java/by/it_academy/jd2/mk_jd2_103_23/group3/messenger/dao/UserDao.java
@@ -0,0 +1,16 @@
+package by.it_academy.jd2.mk_jd2_103_23.group3.messenger.dao;
+
+import by.it_academy.jd2.mk_jd2_103_23.group3.messenger.core.dto.User;
+import by.it_academy.jd2.mk_jd2_103_23.group3.messenger.dao.api.IUserDao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDao implements IUserDao {
+    private final List<User> users = new ArrayList<>();
+
+    @Override
+    public void save(User user) {
+        users.add(user);
+    }
+}

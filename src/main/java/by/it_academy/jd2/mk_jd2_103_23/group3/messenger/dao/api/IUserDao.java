c26a81bd6678c4d0bebcde35b5d48048696ed31c
Dima Luschik <dmitry.luschick@yandex.by>
2023-09-22 23:27:19 +0300

    Added interface IUserDao and class UserDaoFactory

---
+++ src/main/java/by/it_academy/jd2/mk_jd2_103_23/group3/messenger/dao/api/IUserDao.java
@@ -0,0 +1,7 @@
+package by.it_academy.jd2.mk_jd2_103_23.group3.messenger.dao.api;
+
+import by.it_academy.jd2.mk_jd2_103_23.group3.messenger.core.dto.User;
+
+public interface IUserDao {
+    void save(User user);
+}

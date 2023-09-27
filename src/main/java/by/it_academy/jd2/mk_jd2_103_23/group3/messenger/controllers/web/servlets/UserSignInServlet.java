790199712b4c9153a5d07565273fb59a14d69fbd
Dima Luschik <dmitry.luschick@yandex.by>
2023-09-27 22:51:21 +0300

    added signIn.jsp, changed UserSignInServlet

--- src/main/java/by/it_academy/jd2/mk_jd2_103_23/group3/messenger/controllers/web/servlets/UserSignInServlet.java
+++ src/main/java/by/it_academy/jd2/mk_jd2_103_23/group3/messenger/controllers/web/servlets/UserSignInServlet.java
@@ -6,6 +6,7 @@ import by.it_academy.jd2.mk_jd2_103_23.group3.messenger.core.exceptions.SignInEx
 import by.it_academy.jd2.mk_jd2_103_23.group3.messenger.service.api.IUserSignInService;
 import by.it_academy.jd2.mk_jd2_103_23.group3.messenger.service.factory.UserSignInFactory;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,11 +14,18 @@ import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
+@WebServlet(urlPatterns = "/api/login")
 public class UserSignInServlet extends HttpServlet {
     private final static String LOGIN_PARAM_NAME = "login";
     private final static String PASSWORD_PARAM_NAME = "password";
 
     private IUserSignInService userSignInService = UserSignInFactory.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/ui/signIn.jsp").forward(req, resp);
+    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
---
+++ src/main/webapp/ui/signIn.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java"
+contentType="text/html; charset-UTF-8"
+pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Авторизация</title>
+    </head>
+
+    <body>
+        <h1>Авторизация</h1>
+        <form action="${pageContext.request.contextPath}/api/login" method="POST">
+            <fieldset>
+                <legend>Логин и пароль</legend>
+                <div> <input class="form-input" type="text" name="login"
+                placeholder="Введите логин"> </div>
+                <div> <input class="form-input" type="password" name="password"
+                placeholder="Введите пароль"> </div>
+            </fieldset>
+            <button type="submit">Войти</button>
+            <button type="reset">Очистить</button>
+        </form>
+    </body>
+</html>

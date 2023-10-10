efd1ccd6c17f80793153a334a301494b16223525
Mark Miklash <markmiklash@gmail.com>
2023-10-10 16:03:17 +0300

    Add contracts documentation, add servlet packages;

---
+++ src/main/java/by/it_academy/jd2/mk_jd2_103_23/group3/messenger/controllers/web/servlets/ui/UiChatsServlet.java
@@ -0,0 +1,8 @@
+package by.it_academy.jd2.mk_jd2_103_23.group3.messenger.controllers.web.servlets.ui;
+
+
+/**
+ * Is used for set attribute with messages and dispatch in jsp;
+ */
+public class UiChatsServlet {
+}
---
+++ src/main/java/by/it_academy/jd2/mk_jd2_103_23/group3/messenger/controllers/web/servlets/ui/UiSignInServlet.java
@@ -0,0 +1,7 @@
+package by.it_academy.jd2.mk_jd2_103_23.group3.messenger.controllers.web.servlets.ui;
+
+/**
+ * Is used to dispatch to jsp;
+ */
+public class UiSignInServlet {
+}

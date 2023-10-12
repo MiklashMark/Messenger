0d5a9b379f07e4bcaa2d6e25e891aad9471663d4
Dima Luschik <dmitry.luschick@yandex.by>
2023-10-12 22:59:28 +0300

    Default admin initialization, up .gitignore

--- src/main/java/by/it_academy/jd2/mk_jd2_103_23/group3/messenger/controllers/web/listeners/DefaultDataInitializer.java
+++ src/main/java/by/it_academy/jd2/mk_jd2_103_23/group3/messenger/controllers/web/listeners/DefaultDataInitializer.java
@@ -6,6 +6,9 @@ import by.it_academy.jd2.mk_jd2_103_23.group3.messenger.dao.factory.UserDaoFacto
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class DefaultDataInitializer implements ServletContextListener {
 
     // Add default admin initialization to listeners
@@ -13,8 +16,9 @@ public class DefaultDataInitializer implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
         UserDao userDao = UserDaoFactory.getInstance();
 
-        User admin = new User("admin", "admin", true);
+        User admin = new User("admin", "admin", "admin", "admin",
+                LocalDate.of(1969, 1, 1), LocalDateTime.now(),true);
 
-       // userDao.save(admin);
+        userDao.save(admin);
     }
 }
